25bf55afd8db25698b7bebfbf6d5fff280fe644d
Memhir-Yasue <habesha787@gmail.com>
2019-03-14 05:27:55 -0400

    db connected with browser

--- flaskApp.py
+++ flaskApp.py
@@ -1,32 +1,55 @@
 import flask
 import db
-from flask import Flask
+from flask import Flask, render_template, request
 from Ecop import Ecop
 
 def initalizeDB():
+    print(db.db_exists())
     if(db.db_exists()):
         GDP = db.fetch_last_gdp()
+        print("EXISTS!")
         return GDP
     else:
-        Halowa = Ecop(10)
         db.startdb()
         db.connect()
-        GDP = Halowa.gdp_size()
+        GDP = 10
         db.data_entry(GDP)
-        GDP = db.fetch_last_gdp()
-        print(GDP)
         db.closedb()
         return GDP
 
+
+GDP = initalizeDB()
+Account = Ecop(GDP)
 
 server = flask.Flask(__name__)
 #
 # Halowa = Ecop(10)
 # GDP = str(db.fetch_last_gdp())
 @server.route('/')
-def main():
-    GDP = initalizeDB()
-    return str(GDP)
+def home():
+    return render_template('index.html', GDP = GDP)
+
+@server.route('/m', methods=['POST'])
+def update():
+    state = request.form['state']
+    Account.state(int(state))
+    Account.calculate()
+    GDP = str(Account.gdp_size())
+    db.startdb()
+    db.connect()
+    db.data_entry(GDP)
+    db.closedb()
+    print(GDP)
+    return render_template('index.html', GDP = GDP)
+
+# @server.route('/u', methods=['POST'])
+# def update():
+#     state = request.form['state']
+#     Account.state(state)
+#     Account.calculate()
+#     GDP = str(Account.gdp_size())
+#     db.data_entry(GDP)
+#     return render_template('test.html', GDP = GDP)
 
 if __name__ == '__main__':
     server.run(debug=True)
